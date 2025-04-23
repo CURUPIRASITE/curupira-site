d4f279195df768bf5838e3750b5ee935ca8f4c48
Curupira Site <curupiraeucaliptostratados@gmail.com>
2025-04-23 15:57:01 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold text-green-700">Curupira Eucaliptos Tratados</h1>
+      <p className="text-xl mt-4">Site funcionando com React + Vite + Tailwind!</p>
+    </div>
+  );
+}
